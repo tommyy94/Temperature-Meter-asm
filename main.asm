5bcca9d5cc2689df2664d527f2dc7c90965e4591
tommyy94 <tommy574@protonmail.ch>
2018-02-19 15:19:35 +0200

    Commented the code, now using multiple files

--- main.asm
+++ main.asm
@@ -1,13 +1,17 @@
+;******************************************************************************;
+;   HEADER FILES
+;******************************************************************************;
 .nolist
 .include "main.inc"
 .list
 
 
-; ============================================
+;******************************************************************************;
 ;   RESET AND INT VECTORS
-; ============================================
+;******************************************************************************;
 .cseg
-.org $0000
+.org $0000 ;interrupts located at start of the flash
+
 jmp reset               ; Reset
 reti nop                ; INT0
 reti nop                ; INT1
@@ -36,9 +40,9 @@ reti nop                ; TWI
 reti nop                ; SPM_RDY
 
 
-; ============================================
+;******************************************************************************;
 ;   INITIALIZE
-; ============================================
+;******************************************************************************;
 reset: 
     INIT_STACK_POINTER
 
@@ -74,24 +78,12 @@ init_timer2: ;8-bit timer
     ldi tmp_reg0, (1 << OCIE2A)
     sts TIMSK2, tmp_reg0
 
-init_adc:
-    ;using separate voltage source in ADC
-    ldi tmp_reg0, (1 << REFS0)
-    sts ADMUX, tmp_reg0
-
-    ;set division factor between system clock and the ADC to 64, so:
-    ;8MHz/64 = 125kHz
-    ldi tmp_reg0, (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1)
-    sts ADCSRA, tmp_reg0
+    INIT_ADC
 
 init_sleep:
     ;using Power-save Mode
-    ldi tmp_reg0, (1 << SM1) | (1 << SM0) | (1 << SE)
-    sts SMCR, tmp_reg0
-
-init_ports:
-	ldi tmp_reg0, (1 << YELLOW_LED) | (1 << GREEN_LED)
-	out DDRB, tmp_reg0
+    ldi tmp_reg0, (1 << SM1) | (1 << SM0)
+    out SMCR, tmp_reg0
 
 init_PCINT0_vect:
     ;set SYS_RESET_PIN as input and enable it
@@ -110,25 +102,25 @@ init_interrupts:
     sts PCMSK0, tmp_reg0
 
     rcall lcd_init
+    
 
-    sleep
-
-    sei
-
-; ============================================
+;******************************************************************************;
 ;    MAIN LOOP
-; ============================================
+;******************************************************************************;
 main:
     rcall adc_read
     rcall lcd_send_character
-    sleep ;10 ms
-    TOGGLE_BIT YELLOW_LED, PINB, PORTB
+    
+    rcall sleep_10ms
+
 	rjmp main
 
-
-; ============================================
-;   SUBROUTINE: delay_10_us
-; ============================================
+    
+;******************************************************************************;
+; SUBROUTINE: delay_10_us
+; Registers used: tmp_reg0
+; Description: 10 microsecond delay loop.
+;******************************************************************************;
 ;
 ; T = 1/f = 1 / 8 * 10^6 Hz = 0.1250 us/cycle
 ;
@@ -157,194 +149,56 @@ delay_10_us_loop:
     brne delay_10_us_loop                   ;3 * 23 + 1
     nop                                     ;1 cycle
     ret                                     ;4 cycles
-    
-
-; ============================================
-;   SUBROUTINE: adc_read
-; ============================================
-adc_read:
-    ;using channel 0
-    lds tmp_reg0, ADMUX
-    ori tmp_reg0, (1 << MUX0)
-    sts ADMUX, tmp_reg0
-
-    ;start conversion
-    lds tmp_reg0, ADCSRA
-    ori tmp_reg0, (1 << ADSC)
-    sts ADCSRA, tmp_reg0
-
-wait_until_conversion_done:
-    ;ADSC bit is turned off when conversion is done
-    lds tmp_reg0, ADCSRA
-    cpi tmp_reg0, ADSC
-    breq wait_until_conversion_done
-
-    ;load ADC result to 16-bit register
-    lds pointer_reg_low, ADCL
-    lds pointer_reg_high, ADCH
-    ret
-    
-
-; ============================================
-;   SUBROUTINE: lcd_execute_instruction
-; ============================================
-lcd_execute_instruction:
-    push tmp_reg0
-
-    ;set EN HIGH
-    in tmp_reg0, CONTROL_LINES
-    ori tmp_reg0, (1  << EN)
-    out CONTROL_LINES, tmp_reg0
-    ;wait for 2 cycles
-    nop
-    nop
-    ;set EN LOW to execute given instruction
-    in tmp_reg0, CONTROL_LINES
-    andi tmp_reg0, ~(1 << EN)
-    out CONTROL_LINES, tmp_reg0
-
-    pop tmp_reg0
-    ret
 
     
-; ============================================
-;   SUBROUTINE: lcd_wait_if_busy
-; ============================================
-lcd_wait_if_busy:
+;******************************************************************************;
+; SUBROUTINE: sleep_10ms
+; Registers used: tmp_reg0
+; Description: Sleeps for 10 ms. Writes Sleep Enable bit to logic one and sleep 
+; until interrupt occurs, then clears Sleep Enable bit.
+;******************************************************************************;
+sleep_10ms:
     push tmp_reg0
     
-    ;read from data port
-    andi tmp_reg0, ~$FF
-    out DATA_DIRECTION, tmp_reg0
+    sei ;enable interrupts so we can wake up
 
-    ;command mode, read mode
-    in tmp_reg0, CONTROL_LINES
-    andi tmp_reg0, ~(1 << RS)
-    ori tmp_reg0, (1 << RW)
-    out CONTROL_LINES, tmp_reg0
+    ;enable sleep
+    in tmp_reg0, SMCR
+    ori tmp_reg0, (1 << SE)
+    out SMCR, tmp_reg0
 
-keep_waiting:
-    rcall lcd_execute_instruction
-    in tmp_reg0, DATA_LINES
-    cpi tmp_reg0, FIRST_LINE_ADDRESS
-    brsh keep_waiting
-    
-    ;write to data port
-    ori tmp_reg0, $FF
-    out DATA_DIRECTION, tmp_reg0
-
-    ;50 us delay done efficiently
-    rcall delay_10_us
-    rcall delay_10_us
-    rcall delay_10_us
-    rcall delay_10_us
-    rcall delay_10_us
-
-    pop tmp_reg0
-    ret
-    
-
-; ============================================
-;   SUBROUTINE: lcd_send_command
-; ============================================
-lcd_send_command:
-    push tmp_reg0
-
-    rcall lcd_wait_if_busy
-    
-    ;overwrite whole register
-    out DATA_LINES, param_reg0 ;routine parameter
-
-    ;write mode, command mode
-    in tmp_reg0, CONTROL_LINES
-    andi tmp_reg0, ~((1 << RW) | (1 << RS))
-    out CONTROL_LINES, tmp_reg0
-
-    ;send command here
-    rcall lcd_execute_instruction
-
-    ;clear data lines
-    andi tmp_reg0, ~$FF
-    out DATA_LINES, tmp_reg0
-
-    pop tmp_reg0
-    ret
-    
-
-; ============================================
-;   SUBROUTINE: lcd_send_character
-; ============================================
-lcd_send_character:
-    push tmp_reg0
-
-    rcall lcd_wait_if_busy
-    
-    ;load value to datalines, so it can be sent forward
-    ldi tmp_reg0, 'A'
-    out DATA_LINES, tmp_reg0
-    
-    ;read mode, text mode
-    in tmp_reg0, CONTROL_LINES
-    andi tmp_reg0, ~(1 << RW)
-    ori tmp_reg0, (1 << RS)
-    out CONTROL_LINES, tmp_reg0
-
-    ;send character here
-    rcall lcd_execute_instruction
-
-     ;clear data lines
-    andi tmp_reg0, ~$FF
-    out DATA_LINES, tmp_reg0
-
-    ;delay needed after sending character
-    rcall delay_10_us
-
-    pop tmp_reg0
-    ret
-
-
-; ============================================
-;   SUBROUTINE: lcd_init
-; ============================================
-lcd_init:
-    push tmp_reg0
-    
-    ;initialize pins for LCD
-    ;modify instead of overwriting to avoid problems in the future
-    in tmp_reg0, CONTROL_DIRECTION
-    ori tmp_reg0, (1 << EN) | (1 << RW) | (1 << RS)
-    out CONTROL_DIRECTION, tmp_reg0
     sleep
 
-    ldi param_reg0, SET_8_BIT
-    rcall lcd_send_command
-    sleep
-
-    ldi param_reg0, DISPLAY_ON_CURSOR_OFF
-    rcall lcd_send_command
-    sleep
-
-    ldi param_reg0, CLEAR_DISPLAY
-    rcall lcd_send_command
-    sleep
+    ;disable sleep
+    andi tmp_reg0, ~(1 << SE)
+    out SMCR, tmp_reg0
     
     pop tmp_reg0
     ret
 
+    
+;******************************************************************************;
+;   INTERRUPT SERVICE ROUTINES
+;******************************************************************************;
 
-; ============================================
-;   INTERRUPT SERVICES
-; ============================================
-;To be used for watchdog system reset
+;******************************************************************************;
+; PCINT0_vect
+; Registers used: none
+; Description: To be used for watchdog system reset.
+;******************************************************************************;
 PCINT0_vect:
     ;check if SYS_RESET_PIN is pressed
     sbic PINB, SYS_RESET_PIN       ;falling edge
     nop ;debug message: PCINT0
     reti
 
-
+    
+;******************************************************************************;
+; TIM2_COMPA2_vect
+; Registers used: none
+; Description: Used to wake up the processor.
+;******************************************************************************;
 TIM2_COMPA2_vect:
-    TOGGLE_BIT GREEN_LED, PINB, PORTB
     nop ;used to wake up from sleep
     reti
 
